94a82b6b2a1c871a891d07962b453c04d2f9dd7e
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-14 21:10:25 +0300

    add layers

--- app/src/main/java/com/example/gymapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/gymapp/presentation/MainActivity.kt
@@ -1,10 +1,8 @@
-package com.example.gymapp
+package com.example.gymapp.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.gymapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
