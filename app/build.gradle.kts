88cf0c06490a0ca9de4f421cc4c1608626cc9e64
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-14 21:24:18 +0300

    add room dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     id ("kotlin-kapt")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -38,11 +39,19 @@ android {
 
 dependencies {
 
+    val room_version = "2.6.1"
+
     implementation ("com.google.dagger:hilt-android:2.55")
     annotationProcessor ("com.google.dagger:hilt-compiler:2.55")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation ("com.google.dagger:dagger:2.55")
     kapt ("com.google.dagger:dagger-compiler:2.55")
+
+    implementation("androidx.room:room-runtime:$room_version")
+    ksp("androidx.room:room-compiler:2.5.0")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
