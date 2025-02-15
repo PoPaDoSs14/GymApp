07a7615904ae92a8a0ecd14d285aa5336426b7d8
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-15 19:40:24 +0300

    add binding in MainActivity

--- app/src/main/java/com/example/gymapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/gymapp/presentation/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.gymapp.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gymapp.R
+import com.example.gymapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
