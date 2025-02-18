81c9c565c2af220ba882abd6df8c94d39cc926c6
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-18 18:20:02 +0300

    add Exercise id

--- app/src/main/java/com/example/gymapp/data/ExerciseDbModel.kt
+++ app/src/main/java/com/example/gymapp/data/ExerciseDbModel.kt
@@ -1,9 +1,12 @@
 package com.example.gymapp.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity("Exercises")
 data class ExerciseDbModel (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
     val name: String,
     val count: Int
 )
--- app/src/main/java/com/example/gymapp/domain/Exercise.kt
+++ app/src/main/java/com/example/gymapp/domain/Exercise.kt
@@ -1,6 +1,7 @@
 package com.example.gymapp.domain
 
 data class Exercise (
+    val id: Int,
     val name: String,
     val count: Int
 )
