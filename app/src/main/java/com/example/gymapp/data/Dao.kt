10d815522898beefb14d1788513b5e75d4a48113
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-20 19:25:38 +0300

    add new methods in Dao

--- app/src/main/java/com/example/gymapp/data/Dao.kt
+++ app/src/main/java/com/example/gymapp/data/Dao.kt
@@ -1,11 +1,20 @@
 package com.example.gymapp.data
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import com.example.gymapp.domain.Exercise
 
 @Dao
 interface Dao {
 
     @Insert
     fun addExercise(exerciseDbModel: ExerciseDbModel)
+
+    @Delete
+    fun deleteExercise(exercise: Exercise)
+
+    @Query("SELECT * FROM exercises")
+    fun getExercises(): List<Exercise>
 }
