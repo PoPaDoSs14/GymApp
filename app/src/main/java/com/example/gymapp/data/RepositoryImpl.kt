c185d7493436a7f79abc76015d7cea2d53fbabec
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-20 19:19:05 +0300

    add addExercise

--- app/src/main/java/com/example/gymapp/data/RepositoryImpl.kt
+++ app/src/main/java/com/example/gymapp/data/RepositoryImpl.kt
@@ -4,10 +4,14 @@ import android.app.Application
 import com.example.gymapp.domain.Exercise
 import com.example.gymapp.domain.Repository
 
-class RepositoryImpl(private val application: Application): Repository {
+class RepositoryImpl(
+    private val application: Application,
+    private val mapper: ExerciseMapper
+) : Repository {
 
     override suspend fun addExercise(exercise: Exercise) {
-
+        AppDatabase.getInstance(application).Dao()
+            .addExercise(mapper.mapEntityToDbModel(exercise))
     }
 
 }
