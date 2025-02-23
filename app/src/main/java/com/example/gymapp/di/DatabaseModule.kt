7aad43deaba981ae58af8110462fb3821e0cc656
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-23 18:44:02 +0300

    add provideDatabase

--- app/src/main/java/com/example/gymapp/di/DatabaseModule.kt
+++ app/src/main/java/com/example/gymapp/di/DatabaseModule.kt
@@ -1,9 +1,17 @@
 package com.example.gymapp.di
 
+import android.app.Application
+import com.example.gymapp.data.AppDatabase
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class DatabaseModule {
 
-
+    @Provides
+    @Singleton
+    fun provideDatabase(application: Application): AppDatabase {
+        return AppDatabase.getInstance(application)
+    }
 }
