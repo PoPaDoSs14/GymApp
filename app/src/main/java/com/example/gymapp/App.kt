56ea08362e7b883b956040d05154212c2618a931
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-24 20:48:05 +0300

    add component

--- app/src/main/java/com/example/gymapp/App.kt
+++ app/src/main/java/com/example/gymapp/App.kt
@@ -4,5 +4,10 @@ import android.app.Application
 
 class App: Application() {
 
+    val component by lazy {
+        DaggerApplicationComponent
+            .factory()
+            .create()
+    }
 
 }
