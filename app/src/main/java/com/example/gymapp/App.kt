27e9c8e226baf591bf51c94073c28947bbcfb330
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-25 18:45:44 +0300

    delete component

--- app/src/main/java/com/example/gymapp/App.kt
+++ app/src/main/java/com/example/gymapp/App.kt
@@ -4,10 +4,6 @@ import android.app.Application
 
 class App: Application() {
 
-    val component by lazy {
-        DaggerApplicationComponent
-            .factory()
-            .create()
-    }
+
 
 }
